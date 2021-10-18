294519548b3e81b6b42480730521a929e70854a3
pawadenilesh <npawade@key2act.com>
2021-10-18 22:27:57 +0530

    initial setup done

--- akm-crm/src/main/java/Base/BaseClass.java
+++ akm-crm/src/main/java/Base/BaseClass.java
@@ -1,11 +1,23 @@
 package Base;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import io.github.bonigarcia.wdm.config.OperatingSystem;
+
 public class BaseClass {
+	public static WebDriver driver;
+	@BeforeClass
+	public void setup() {
+		WebDriverManager.chromedriver().operatingSystem(OperatingSystem.WIN).setup();
+		driver = new ChromeDriver();
+	}
 
 	@Test
 	public void sampletest() {
-		System.out.println("Sample Test");
+		driver.get("https://akmcrmqa-apps.deeccus.com/app/index.html#/login");
 	}
 }
